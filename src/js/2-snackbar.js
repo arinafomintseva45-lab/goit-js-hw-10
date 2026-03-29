d1e57c95bcec63cc3b91334665e7b26c75684613
arinafomintseva45-lab <arinafomintseva45@gmail.com>
2026-03-29 17:56:36 +0200

    ok

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const delayInput = form.elements["delay"].value;
+  const stateInput = form.elements["state"].value;
+
+  const delay = Number(delayInput);
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === "fulfilled") {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  }
+  createPromise(delay, stateInput)
+    .then((delay) => {
+      iziToast.success({
+        title: "✅ Fulfilled",
+        message: `Fulfilled promise in ${delay}ms`,
+        position: "topRight",
+        timeout: 3000
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: "❌ Rejected",
+        message: `Rejected promise in ${delay}ms`,
+        position: "topRight",
+        timeout: 3000
+      });
+    });
+  form.reset();
+});
